8ffe2c6e3050651b15ea6182e997641bbbeeca96
JosephLi <williamlifaith@gmail.com>
2018-03-27 13:25:05 -0500

    upload to deploy to hosting

--- blog/admin.py
+++ blog/admin.py
@@ -7,13 +7,19 @@ from .models import Poem
 @admin.register(BlogType)
 class BlogTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'type_name')
+    list_filter = ('id', 'type_name')
+    search_fields = ['type_name']
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'blog_type', 'get_read_num', 'author', 'created_time', 'last_updated_time')
+    list_filter = ('id', 'title', 'blog_type', 'author', 'created_time', 'last_updated_time')
+    search_fields = ['title', 'blog_type', 'get_read_num', 'author', 'created_time', 'last_updated_time']
 
 
 @admin.register(Poem)
 class PoemAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'author', 'created_time', 'last_updated_time')
+    list_filter = ('id', 'title', 'content', 'author', 'created_time', 'last_updated_time')
+    search_fields = ['title', 'author']
